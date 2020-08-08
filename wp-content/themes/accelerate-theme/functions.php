1837075c820527b3a6d6dfdf5f9284ecab5920c6
kristin grigsby <kristingrigsby@yahoo.com>
2020-08-07 22:35:53 -0400

    I have no idea why it looks like I've touched every document

--- wp-content/themes/accelerate-theme/functions.php
+++ wp-content/themes/accelerate-theme/functions.php
@@ -86,18 +86,3 @@ function custom_excerpt_more($more) {
 }
 add_filter('excerpt_more', 'custom_excerpt_more');
 
-//Custom posts
-function create_custom_post_types() {
-    register_post_type( 'case_studies',
-        array(
-            'labels' => array(
-                'name' => __( 'Case Studies' ),
-                'singular_name' => __( 'Case Study' )
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array( 'slug' => 'case-studies' ),
-        )
-    );
-}
-add_action( 'init', 'create_custom_post_types' );
